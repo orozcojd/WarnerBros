139a17a612f3a229ad0b7b495dca2aed7d40d477
Jonathan Orozco <socaljorozco@gmail.com>
2020-02-14 15:16:54 -0800

    added api token for external use.

--- src/common/config.js
+++ src/common/config.js
@@ -1,4 +1,4 @@
 export const API_URL = 'https://api.themoviedb.org/3/'
 export const API_IMAGE_URL = 'https://image.tmdb.org/t/p/w500/'
-export const API_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNmViMjQxYmFjZGFiM2FkOTNjNGMyODFlMDcxMjcyOSIsInN1YiI6IjVlNDVkNTcyOTYwMzMxMDAxOWY1OTMxMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.b8GOmhiNfJWx_jJU4rxvKQl1TA6ag2xmNxDVLIorzPc'
+export const API_TOKEN = process.env.API_TOKEN || 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTUxZWQ5ZjQzOWM2YmFkODNkNzc5ZjY4YjlhZjY4NCIsInN1YiI6IjVlNDcyOGFkMmQ5Mzc1MDAxM2E0YzkzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OPAsyFyvlOpUwceB1trbAAg5AlQ6loW3Wc3qVnbS - Os'
 export default API_URL
